c81f64f1c600f731e09a3a5eb888fe492e24c02a
shrikantdande <shriiikant@gmail.com>
2024-07-03 20:38:50 +0530

    product of array except self

---
+++ src/main/java/problemsolving/ProductOfArrayExceptSelf.java
@@ -0,0 +1,35 @@
+package problemsolving;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ProductOfArrayExceptSelf {
+    public static void main(String[] args) {
+        int[] result = findProduct(new int[]{1, 5, -2, 4});
+        Arrays.stream(result).forEach(System.out::println);
+    }
+    public static int[] findProduct(int arr[]) {
+
+            int[] products = new int[arr.length];
+            int leftProduct =1;
+            for(int i=0; i< arr.length; i++){
+                int product = 1;
+                // product of right elements
+                for(int k=i+1; k< arr.length; k++){
+                    product *= arr[k];
+                }
+                //product of left elements
+                // for(int l =0; l <i+1; l++){
+                //     if(l != i){
+                //     product *= arr[l];
+                //     }
+                // }
+
+                products[i] = product * leftProduct;
+                leftProduct *= arr[i];
+
+            }
+            return products;
+        }
+    }
+
